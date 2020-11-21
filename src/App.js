b66c104cf3a5d74f3062c8ac4d4fe83c29f00488
Scott McClellan <scott.t.mcclellan95@gmail.com>
2020-11-21 17:44:40 -0600

    Code at the end of Section3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Person from './Person/Person';
 
 function App() {
+  const [persons, setPersons] = useState([
+    { name: 'Max', age: 28 },
+    { name: 'Manu', age: 29 },
+    { name: 'Steph', age: 26 }
+  ]);
+
+  const updatePersonsName = (index, name) => {
+    let newPersons = [...persons];
+    newPersons[index].name = name;
+    setPersons(newPersons);
+  };
+
+  const updatePersonsAge = (index, age) => {
+    let newPersons = [...persons];
+    newPersons[index].age = age;
+    setPersons(newPersons);
+  };
+
+  const switchNameHandler = () => {
+    updatePersonsName(0, 'Maximilian');
+    updatePersonsAge(2, 37);
+  };
+
+  const onChangeHandler = (event) => {
+    updatePersonsName(1, event.target.value);
+  }
+
+  const style = {
+    backgroundColor: 'white',
+    font: 'inherit',
+    border: '1px solid blue',
+    padding: '8px',
+    cursor: 'pointer'
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>Hi, I'm a react app</h1>
+      <button style={style} onClick={switchNameHandler}>Switch Name</button>
+      <Person
+        name={persons[0].name}
+        age={persons[0].age}
+        onClickHandler={() => updatePersonsName(0, 'Max0')}
+        changed={onChangeHandler} >My Hobby is: Racing</Person>
+      <Person
+        name={persons[1].name}
+        age={persons[1].age}
+        onClickHandler={switchNameHandler}
+        changed={onChangeHandler} >My Hobby is: Horseback Riding</Person>
+      <Person
+        name={persons[2].name}
+        age={persons[2].age}
+        updateAgeHandler={updatePersonsAge}
+        onClickHandler={() => { updatePersonsName(0, 'Max2'); updatePersonsAge(2, 27); }}
+        changed={onChangeHandler} >My Hobby is: Coloring</Person>
+    </div >
+    //React.createElement('div', {className: 'App' }, React.createElement('h1', null, 'Hi, I\'m a react app!'))
   );
 }
+
+
+
+
 
 export default App;
